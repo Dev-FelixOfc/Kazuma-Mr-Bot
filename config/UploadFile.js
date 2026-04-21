18b9994ad96eb88f51196002a6441850a1ebb89e
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-21 00:19:33 -0400

    Actualizar UploadFile.js

--- config/UploadFile.js
+++ config/UploadFile.js
@@ -1,10 +1,18 @@
 import axios from 'axios';
 import FormData from 'form-data';
+import { fileTypeFromBuffer } from 'file-type';
 
 export const uploadToYotsuba = async (buffer) => {
     try {
+        const type = await fileTypeFromBuffer(buffer);
+        const extension = type ? type.ext : 'jpg';
+        const mimetype = type ? type.mime : 'image/jpeg';
+
         const form = new FormData();
-        form.append('file', buffer, { filename: 'yotsuba_upload.jpg' });
+        form.append('file', buffer, { 
+            filename: `kazuma_upload.${extension}`,
+            contentType: mimetype 
+        });
 
         const response = await axios.post('https://upload.yotsuba.giize.com/upload', form, {
             headers: {
@@ -14,7 +22,6 @@ export const uploadToYotsuba = async (buffer) => {
 
         return response.data.url; 
     } catch (error) {
-        console.error('Error en Yotsuba Upload Service:', error);
-        throw new Error('No se pudo subir el archivo a la nube.');
+        throw new Error('Fallo al subir el archivo.');
     }
 };
