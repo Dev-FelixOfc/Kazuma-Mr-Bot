b7e49aa189c35426357fafc69f8e4f16ab31d1bb
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-21 09:20:33 -0400

    Eliminar visuals_context.js

--- config/visuals_context.js
+++
@@ -1,7 +0,0 @@
-export const contextConfig = {
-    title: 'KAZUMA - ECONOMY SYSTEM',
-    body: 'Desarrollado por PixelCrew-Team',
-    image: 'https://files.catbox.moe/9ssbf9.jpg', // Tu imagen principal
-    link: 'https://www.instagram.com/dev.felixofc',
-    canal: 'https://whatsapp.com/channel/0029VajV9sCDzgTBr89Xv001'
-};
