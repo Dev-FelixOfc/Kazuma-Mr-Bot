95f3a68d6c43f824cd1677d7555d0f2afce0b36c
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-24 02:40:54 -0400

    Actualizar profile-delgenre.js

--- comandos/profile-delgenre.js
+++ comandos/profile-delgenre.js
@@ -16,11 +16,11 @@ const delGenre = {
             if (!fs.existsSync(genrePath)) return m.reply(`*${config.visuals.emoji2}* Sin registros.`);
             
             let genres = JSON.parse(fs.readFileSync(genrePath, 'utf-8'));
-            if (!genres[user]) return m.reply(`*${config.visuals.emoji2}* No posees un género establecido.`);
+            if (!genres[user]) return m.reply(`*${config.visuals.emoji2} \`DATO INEXISTENTE\` ${config.visuals.emoji2}*\n\nNo posees un género establecido para borrar.\n\n> ¡Usa #setgenre para registrarte!`);
 
             delete genres[user];
             fs.writeFileSync(genrePath, JSON.stringify(genres, null, 2));
-            m.reply(`*${config.visuals.emoji3}* \`GÉNERO ELIMINADO\` 🗑️`);
+            m.reply(`*${config.visuals.emoji3} \`GÉNERO PURGADO\` ${config.visuals.emoji3}*\n\nTu identidad ha sido eliminada de la base de datos 🗑️\n\n> ¡Puedes volver a elegir un género cuando gustes!`);
         } catch (e) {
             m.reply('✘ Error al purgar identidad.');
         }
