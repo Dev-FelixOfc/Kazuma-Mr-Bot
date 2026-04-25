234a36d91eed80d7a917a38ef431976662e9de88
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-25 09:07:03 -0400

    Actualizar stickers-brat.js

--- comandos/stickers-brat.js
+++ comandos/stickers-brat.js
@@ -18,20 +18,21 @@ const bratCommand = {
             const canvas = new Jimp(512, 512, 0xFFFFFFFF);
             const font = await Jimp.loadFont(Jimp.FONT_SANS_64_BLACK);
 
-            const textWidth = Jimp.measureText(font, text);
-            const textHeight = Jimp.measureTextHeight(font, text, 512);
+            const margin = 20;
+            const maxWidth = 512 - (margin * 2);
+            const maxHeight = 512 - (margin * 2);
 
             canvas.print(
                 font,
-                (512 - textWidth) / 2,
-                (512 - textHeight) / 2,
+                margin,
+                margin,
                 {
                     text: text,
                     alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
                     alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
                 },
-                512,
-                512
+                maxWidth,
+                maxHeight
             );
 
             const buffer = await canvas.getBufferAsync(Jimp.MIME_PNG);
