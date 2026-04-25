4bf7797cb0e3f00c69fb74637ea0225c159a6e64
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-25 02:39:02 -0400

    Crear config.js

---
+++ config/config.js
@@ -0,0 +1,10 @@
+export const configPriority = {
+    stickers: {
+        packname: 'Tu Packname Aquí',
+        packauthor: 'Tu Author Aquí',
+    },
+    system: {
+        priority: true,
+        version: '1.1.0'
+    }
+};
