6d39a0d92aad813b56ef324f5d8ceee3aaf91b96
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-27 18:38:10 -0400

    Actualizar stickers-s.js

--- comandos/stickers-s.js
+++ comandos/stickers-s.js
@@ -1,5 +1,5 @@
 import { config } from '../config.js';
-import { configPriority } from '../config/config.js';
+import { getDynamicConfig } from '../config/config.js';
 import { Sticker, StickerTypes } from 'wa-sticker-formatter';
 
 const stickerCommand = {
@@ -21,13 +21,11 @@ const stickerCommand = {
             let img = await q.download();
             if (!img) return m.reply(`*${config.visuals.emoji2}* Error al descargar.`);
 
+            const dynamicConfig = await getDynamicConfig(conn);
             let userName = m.pushName || 'User';
-            let botName = config.botName || 'Kazuma Bot';
 
-            let pack = configPriority.stickers.packname;
-            let author = configPriority.stickers.packauthor
-                .replace('(botName)', botName)
-                .replace('(userName)', userName);
+            let pack = dynamicConfig.stickers.packname;
+            let author = dynamicConfig.stickers.packauthor.replace('(userName)', userName);
 
             let sticker = new Sticker(img, {
                 pack: pack,
@@ -42,7 +40,6 @@ const stickerCommand = {
             await conn.sendMessage(m.chat, { sticker: buffer }, { quoted: m });
 
         } catch (e) {
-            console.error(e);
             m.reply(`*${config.visuals.emoji2}* Error interno.`);
         }
     }
